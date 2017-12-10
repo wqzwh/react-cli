4a414a9fcddc461d36dbcb7f8860ee643e3bf295
wangqi773 <wangqi29@jd.com>
2017-12-10 22:29:18 +0800

    添加路由配置

--- src/router/index.js
+++ src/router/index.js
@@ -14,6 +14,7 @@ class Routers extends Component {
     return(
       <Router>
         <Switch>
+          <Route path="/" component={Demo} />
           <Route path="/demo" component={Demo} />
           <Route path="/button" component={Button} />
         </Switch>
--- src/views/button/index.js
+++ src/views/button/index.js
@@ -4,7 +4,7 @@ class Button extends Component {
   render() {
     return (
       <div>
-        button
+        wangqi
       </div>
     );
   }
